32fe5cd1994c14fe63f45fdb3d61b59ac6775fde
mojumah <jumah.mohammad@gmail.com>
2018-10-18 22:54:29 +0100

    adding aws_key_pair resource, after using ssh-keygen -f

--- example.tf
+++ example.tf
@@ -4,7 +4,17 @@ provider "aws" {
   region     = "us-east-1"
 }
 
+resource "aws_key_pair" "mo-key" {
+  key_name   = "mo-key"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQD1FGtYkhmvCAkcGoCagZTmblBbgOGCTMDHuCd/rd9MwvCChiaCPnD5lMKxYx8gAJgjwocQeRQTHSm+ohNptZAtoUUjFb8FVczqVKe7tS6XGyLbjrgafKHA14ApyVDfOHl5TOMQQP4EzrgNsLX6ZxlGKJAfl7pwD+DF6p4VIfoXX9kC1OwSby9jP0mYxsSEOfuFHJIxhHqVZDiFXJHVGLBXtJkwSTjGemVJygsnJfZ0xnOF0AOfVekiOeJ01BBsFxiMWndIVMMjE3ZpXfjOqxivLASoy5GnfAwGpZK0MUfh1ZgBxdjxiBYU9xl3EaSnMMYKaq9pKbvt+/hKYiqTHF0x root@ubuntu"
+
+}
+
 resource "aws_instance" "example" {
   ami           = "ami-2757f631"
   instance_type = "t2.micro"
+  key_name = "${aws_key_pair.mo-key.key_name}"
+  tags {
+      Name = "Test instance"
+  }
 }
